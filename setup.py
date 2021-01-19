06ace6142d10ae6c0cf34fd644ad6f32780eacd9
angel gutarra <agutarr2@gmu.edu>
2021-01-19 03:19:58 -0500

    changed import util in setup.py back to original state

--- setup.py
+++ setup.py
@@ -26,10 +26,8 @@ if len(sys.argv) > 1 and sys.argv[1] == 'develop':
 
 # https://stackoverflow.com/a/8663557
 # Temporarily modify path so that util functions can be used.
-setup_path = os.path.dirname(os.path.realpath(__file__))
-print(setup_path)
-sys.path.insert(0, os.path.join(setup_path, 'magnetovis'))
-print(sys.path[0])
+setup_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, os.path.join(setup_path, 'magnetovis', 'magnetovis'))
 import util
 del sys.path[0]
 
