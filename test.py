95cfbc14eff29d36b6e5a0e60bb3aef1382c25ad
Bob Weigel <rweigel@gmu.edu>
2021-07-06 14:12:04 -0400

    add test file

---
+++ test.py
@@ -0,0 +1,22 @@
+"""
+Usage:
+    magnetovis --script=test.py
+"""
+
+import magnetovis as mvs
+
+time = [2015, 1, 1, 0, 0]
+csys = 'GEO'
+
+mvs.plane(time=time, val='XY', extend=[[-55, 25], [-55, 55]], coord_sys=csys)
+mvs.plane(time=time, val='XZ', extend=[[-55, 25], [-55, 55]], coord_sys=csys)
+mvs.plane(time=time, val='YZ', extend=[[-55, 55], [-55, 55]], coord_sys=csys)
+
+_, _, xAxisSource = mvs.axis(time=time, val='X', lims=[-15,15], coord_sys=csys)
+mvs.tube(xAxisSource, vary_radius='By Scalar', tube_radius=0.05)
+
+_, _, yAxisSource = mvs.axis(time=time, val='Y', lims=[-15,15], coord_sys=csys)
+mvs.tube(yAxisSource, vary_radius='By Scalar', tube_radius=0.05)
+
+_, _, zAxisSource = mvs.axis(time=time, val='Z', lims=[-15,15], coord_sys=csys)
+mvs.tube(zAxisSource, vary_radius='By Scalar', tube_radius=0.05)
