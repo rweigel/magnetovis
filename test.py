98c4b512865853f1ae37e7667cc83b4de5ac634b
Angel <agutarr2@physicsminsimac.myfiosgateway.com>
2021-07-30 15:26:19 -0400

    used hxform and pvs in test.py to set camera and axis of rotation in the correct place for any hxform transformation.

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ Usage:
 """
 
 import magnetovis as mvs
+import paraview.simple as pvs
+from hxform import hxform as hx
 
 time = [2015, 1, 1, 0, 0]
-csys = 'GSM'
+csys = 'SM'
 
-#mvs.earth(time, coord_sys=csys)
+mvs.earth(time, coord_sys=csys)
 
 mvs.plane(time=time, val='XY', extend=[[-55, 25], [-55, 55]], coord_sys=csys)
 mvs.plane(time=time, val='XZ', extend=[[-55, 25], [-55, 55]], coord_sys=csys)
@@ -22,3 +24,29 @@ mvs.tube(yAxisSource, vary_radius='By Scalar', tube_radius=0.05)
 
 _, _, zAxisSource = mvs.axis(time=time, val='Z', lims=[-55, 55], coord_sys=csys)
 mvs.tube(zAxisSource, vary_radius='By Scalar', tube_radius=0.05)
+
+
+if csys != 'GSM':
+    gsm_text = pvs.Text(registrationName='GSM')
+    gsm_text.Text = 'GSM'
+    renderView = pvs.GetActiveViewOrCreate('RenderView')
+    gsm_text_display = pvs.Show(gsm_text)
+    gsm_text_display.WindowLocation = 'AnyLocation'
+    gsm_text_display.Position = [0.04, 0.04]
+    gsm_text_display.Interactivity = 0
+
+# setting up proper viewing properties
+cam_pos = [180,250,100]
+cam_view_up = [0,0,1]
+cam = pvs.GetActiveCamera()
+if csys != 'GSM':
+    cam_view_up = hx.transform(cam_view_up, time, 'GSM', csys, 'car', 'car')
+    cam_pos = hx.transform(cam_pos, time, 'GSM', csys, 'car', 'car')
+cam.SetPosition(*cam_pos)
+cam.SetViewUp(*cam_view_up)
+cam.SetFocalPoint(0,0,0)
+pvs.Render()
+
+# setting the center of rotation
+view = pvs.GetActiveViewOrCreate("RenderView")
+view.CenterOfRotation = [0,0,0]
