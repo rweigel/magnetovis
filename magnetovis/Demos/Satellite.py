255740295cbb809c923a4258c9e285d776a297e8
Angel <agutarr2@physicsminsimac.myfiosgateway.com>
2021-10-23 12:35:13 -0400

    added tube options to satellite function.

--- magnetovis/Demos/Satellite.py
+++ magnetovis/Demos/Satellite.py
@@ -10,8 +10,10 @@ import magnetovis as mvs
 
 sourceArguments = {
                     "time_o": "2001-01-01",
-                    "time_f": "2001-01-02",
-                    "coord_sys": "GSM"
+                    "time_f": "2001-01-03",
+                    "coord_sys": "GSM",
+                    "satellite_id": 'geotail',
+                    'tube_radius': None
                 }
 
 displayArguments = {
@@ -42,21 +44,21 @@ Satellite = mvs.Satellite(
                     renderSource=True,
                     displayArguments=displayArguments # Ignored if renderSource=False
                 )
+if False:
+    print(Satellite.programmableSource)
+    print(Satellite.displayProperties)   # None b/c renderSource was False
+    print(Satellite.renderView)          # None b/c renderSource was False
+    print(Satellite.displayArguments)
+    print(Satellite.sourceArguments)
 
-print(Satellite.programmableSource)
-print(Satellite.displayProperties)   # None b/c renderSource was False
-print(Satellite.renderView)          # None b/c renderSource was False
-print(Satellite.displayArguments)
-print(Satellite.sourceArguments)
+    displayArguments['showSource'] = True
+    displayArguments['ambientColor'] = [1, 0, 0]
+    displayArguments['diffuseColor'] = [1, 0, 0]
+    Satellite.SetDisplayOptions(displayArguments)
 
-displayArguments['showSource'] = True
-displayArguments['ambientColor'] = [1, 0, 0]
-displayArguments['diffuseColor'] = [1, 0, 0]
-Satellite.SetDisplayOptions(displayArguments)
-
-print(Satellite.programmableSource)
-print(Satellite.displayProperties)
-print(Satellite.renderView)
+    print(Satellite.programmableSource)
+    print(Satellite.displayProperties)
+    print(Satellite.renderView)
 
 #import paraview.simple as pvs
 #pvs.SaveScreenshot('Axis_demo.png', Axis.renderView, ImageResolution=[1670, 1091])
--- magnetovis/Objects/Satellite.py
+++ magnetovis/Objects/Satellite.py
@@ -11,13 +11,17 @@ def ScriptRequestInformation(self):
    # What is entered in the Script (RequestInformation) box for a Programmable Source
    pass
 
-def Script(self, time_o="2001-01-01", time_f="2001-01-02", satellite_id='ace', coord_sys='GSM'):
+def Script(self, time_o="2001-01-01", time_f="2001-01-02", satellite_id='ace', coord_sys='GSM', tube_radius=1.):
 
     import vtk
     import numpy as np
+    from itertools import groupby
+
 
     from hxform import hxform as hx
     from hapiclient import hapi
+    from vtk.numpy_interface import dataset_adapter as dsa
+
     region_id = {
                           'D_Msheath' : 0,
                           'N_Msheath' : 1,
@@ -40,33 +44,43 @@ def Script(self, time_o="2001-01-01", time_f="2001-01-02", satellite_id='ace', c
         output = vtkPolyData.GetData(outInfo, 0)
 
     server     = 'http://hapi-server.org/servers/SSCWeb/hapi';
-    opts       = {'logging': True, 'usecache': True}
+    opts       = {'logging': False, 'usecache': True}
     parameters = "X_{},Y_{},Z_{},Spacecraft_Region" \
                 .format(coord_sys, coord_sys, coord_sys)
     data, meta = hapi(server, satellite_id, parameters, time_o, time_f, **opts)
 
-    output.Allocate(len(data), 1)
-    pts = vtk.vtkPoints()
-    polyline = vtk.vtkPolyLine()
-    polyline.GetPointIds().SetNumberOfIds(len(data['Spacecraft_Region']))
+    points = np.column_stack([data['X_'+coord_sys], data['Y_'+coord_sys], data['Z_'+coord_sys]])
+    pvtk = dsa.numpyTovtkDataArray(points)
 
-    for i in range(len(data['Spacecraft_Region'])):
-        pts.InsertPoint(i,data['X_'+coord_sys][i], data['Y_'+coord_sys][i],
-                          data['Z_'+coord_sys][i])
-        polyline.GetPointIds().SetId(i,i)
-    output.InsertNextCell(polyline.GetCellType(), polyline.GetPointIds())
+    pts = vtk.vtkPoints()
+    pts.Allocate(points.shape[0])
+    pts.SetData(pvtk)
+    output.Allocate(points.shape[0])
     output.SetPoints(pts)
 
+    polyline = vtk.vtkPolyLine()
     vtk_region_id = vtk.vtkIntArray()
     vtk_region_id.SetNumberOfComponents(1)
-    vtk_region_id.SetName(satellite_id + ' Spacecraft Region')
+    vtk_region_id.SetName('region_id')
+    point_id = 0
+    for region, group in groupby(data['Spacecraft_Region']):
+        vtk_region_id.InsertNextTuple([region_id[region.decode('UTF-8')]])
+        group = list(group)
+        polyline.GetPointIds().SetNumberOfIds(len(group))
+        for line_id in range(len(group)):
+            polyline.GetPointIds().SetId(line_id, point_id)
+            point_id += 1
+        output.InsertNextCell(polyline.GetCellType(), polyline.GetPointIds())
+    output.GetCellData().AddArray(vtk_region_id)
 
-    for region in data['Spacecraft_Region']:
-        if region_id == None:
-            vtk_region_id.InsertNextTuple([0])
-        else:
-            vtk_region_id.InsertNextTuple([region_id[region.decode('UTF-8')]])
-    output.GetPointData().AddArray(vtk_region_id)
+    if tube_radius:
+        vtkTubeFilter = vtk.vtkTubeFilter()
+        vtkTubeFilter.SetNumberOfSides(10)
+        vtkTubeFilter.SetInputData(output)
+        vtkTubeFilter.SetRadius(tube_radius)
+        vtkTubeFilter.Update()
+        vtkTubeFilterOutput = vtkTubeFilter.GetOutputDataObject(0)
+        output = output.DeepCopy(vtkTubeFilterOutput)
 
 def Display(source, display, renderView, **displayArguments):
 
@@ -74,6 +88,7 @@ def Display(source, display, renderView, **displayArguments):
 
     import paraview
     import paraview.simple as pvs
+    import numpy as np
 
     region_colors = {
         'D_Msheath' : (230./255, 25./255,  75./255,  0.7), # red
@@ -91,7 +106,7 @@ def Display(source, display, renderView, **displayArguments):
 
     # Over-ride default based on input
     if "region_colors" in displayArguments:
-        for idx, name in enumerate(region_colors.items()):
+        for name in region_colors.keys():
             if name in displayArguments['region_colors']:
                 region_colors[name] = displayArguments['region_colors'][name]
 
@@ -100,34 +115,30 @@ def Display(source, display, renderView, **displayArguments):
                 'HLB_Layer', 'LLB_Layer', 'Intpl_Med']
 
 
-    if False:
 
-        LUT = pvs.GetColorTransferFunction('region_id')
-        LUT.InterpretValuesAsCategories = 1
-        LUT.AnnotationsInitialized = 1
+    LUT = pvs.GetColorTransferFunction('region_id')
+    LUT.InterpretValuesAsCategories = 1
+    LUT.AnnotationsInitialized = 1
 
 
-        for id in id_names:
-            color = region_colors[id_names[id]][0:3]
-            # Set color in lookup table
-        annotations = []
-        index_colored_list = []
-        for id in range(len(id_names)):
-            annotations.append(str(id))
-            annotations.append(unique_regions[i])
-            if kwargs['region_colors'] != None:
-                index_colored_list.append(region_colors[id][0:3])
-            else:
-                index_colored_list.append(kwargs['color'][0:3])
+    annotations = []
+    index_colored_list = []
+    for id, name in enumerate(id_names):
+        annotations.append(str(id))
+        annotations.append(name)
+        if region_colors != None:
+            index_colored_list.append(region_colors[name][0:3])
+        else:
+            index_colored_list.append(region_colors[name][0:3])
 
-        LUT.Annotations = annotations
-        index_colored_list = np.array(index_colored_list).flatten()
-        LUT.IndexedColors = index_colored_list
+    LUT.Annotations = annotations
+    index_colored_list = np.array(index_colored_list).flatten()
+    LUT.IndexedColors = index_colored_list
 
-        programmableSourceDisplay.LookupTable = LUT
-        programmableSourceDisplay.OpacityArray = ['POINTS', scalar_data]
-        programmableSourceDisplay.ColorArrayName = ['POINTS', scalar_data]
-        programmableSourceDisplay.SetScalarBarVisibility(renderView, True)
+    display.LookupTable = LUT
+    display.OpacityArray = ['CELLS', 'region_id']
+    display.ColorArrayName = ['CELLS', 'region_id']
+    display.SetScalarBarVisibility(renderView, True)
 
     #pvs.ColorBy(display, ('CELLS', 'region_id'))
     #lookupTable = pvs.GetColorTransferFunction('region_id')
