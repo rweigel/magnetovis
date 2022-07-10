8357fedc5f9e7a688937375b220e7fbba7baf9fd
Bob Weigel <rweigel@gmu.edu>
2022-07-10 05:44:42 -0400

    rm files

--- magnetovis/Test/bug_mathtext.py
+++
@@ -1,7 +0,0 @@
-# https://matplotlib.org/stable/tutorials/text/mathtext.html
-#  "Regular text and mathtext can be interleaved within the same string."
-
-# Enter in the Python Shell
-view = GetActiveViewOrCreate('RenderView')
-text = Text(Text=r"Plot of $\alpha^2$")
-Show(text)
