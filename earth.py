d43aef341b737b0439402874c28c8d22398f617e
Bob Weigel <rweigel@gmu.edu>
2022-09-13 13:48:18 -0400

    earth.py

--- earth.py
+++ earth.py
@@ -1,10 +1,20 @@
 # Demo 1
 import magnetovis as mvs
-#mvs.ClearPipeline()
-#(X, Y, Z) = [0.7116504060900085, -0.6711866226392706, 0.20751437806093234]
+
+mvs.ClearPipeline()
+
+# Geographic
+mvs.Earth(time='2001-01-01', coord_sys="GEO")
 (X, Y, Z) = (0.2795167448086866, 0.903810269383695, 0.32403300191204026)
-mvs.Earth(time='2019-09-02T04:10:00')
-#mvs.SetTitle("")
-#import paraview.simple as pvs
-#s = pvs.Sphere(Radius=0.1, Center=(X,Y,Z))
-#pvs.Show(s)
+import paraview.simple as pvs
+s = pvs.Sphere(Radius=0.1, Center=(X,Y,Z))
+pvs.Show(s)
+
+if False:
+  # GSM
+  mvs.Earth(time='2001-01-01', coord_sys="GSM")
+  (X, Y, Z) = [-0.3963430948923141, -0.9180665499032108, 0.008121642690819327]
+  import paraview.simple as pvs
+  s = pvs.Sphere(Radius=0.1, Center=(X,Y,Z))
+  pvs.Show(s)
+
