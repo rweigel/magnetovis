4bf0f8bfd3038e23283967c1759acafe2303b844
Bob Weigel <rweigel@gmu.edu>
2022-12-12 13:43:16 -0500

    NEZ_demo improvements

--- magnetovis/Demos/NEZ_demo.py
+++ magnetovis/Demos/NEZ_demo.py
@@ -4,7 +4,18 @@ import magnetovis as mvs
 import numpy as np
 
 def nez(time, pos, csys):
-  """Unit vectors in geographic north, east, and zenith dirs"""
+  """Unit vectors in geographic north, east, and zenith dirs
+
+    Given a cartesian position `pos` in `csys` at time `time`,
+    returns the cartesian unit vectors for geographic north,
+    east, and zenith in `csys`.
+
+    Example:
+      n_geo, e_geo, z_geo = nez("2010-01-01T00:00:00", (1, 0, 0), "GEO")
+      print("North: {}".format(n_geo)) # North: [0. 0. 1.]
+      print("East:  {}".format(e_geo)) # East:  [0. 1. 0.]
+      print("Z:     {}".format(z_geo)) # Z:     [1. 0. 0.]
+  """
 
   from hxform import hxform as hx
 
@@ -20,14 +31,15 @@ def nez(time, pos, csys):
   n_geo = np.cross(z_geo, e_geo)
   n_geo = n_geo/np.linalg.norm(n_geo)
 
-  print(f"Unit vectors for Geographic N, E, and Z at {pos} in {csys} at {time}:")
-  print("North: {}".format(n_geo))
-  print("East:  {}".format(e_geo))
-  print("Z:     {}".format(z_geo))
+  if False:
+    print(f"Unit vectors for Geographic N, E, and Z at {pos} in {csys} at {time}:")
+    print("North: {}".format(n_geo))
+    print("East:  {}".format(e_geo))
+    print("Z:     {}".format(z_geo))
 
   return n_geo, e_geo, z_geo
 
-def showaxis(pos, n_geo, e_geo, z_geo):
+def showaxis(pos, n_geo, e_geo, z_geo, pos_label=""):
 
   def showlabel(dir,base,top):
 
@@ -35,51 +47,58 @@ def showaxis(pos, n_geo, e_geo, z_geo):
     text1Display = pvs.Show(Label, pvs.GetActiveViewOrCreate('RenderView'), 'TextSourceRepresentation',
       TextPropMode='Flagpole Actor',BasePosition=base,TopPosition=top)
 
-  north = pvs.Line(registrationName="N", Point1=pos, Point2=pos+n_geo)
-  north_tube = pvs.Tube(north, Radius=0.02)
+  north = pvs.Line(registrationName="  N_GEO Axis Line", Point1=pos, Point2=pos+n_geo)
+  north_tube = pvs.Tube(north, registrationName="  N_GEO Axis Tube", Radius=0.02)
   mvs.SetColor('blue')
   pvs.Show(north_tube)
-  showlabel('N',pos+n_geo,pos+1.05*n_geo)
+  showlabel('N', pos+n_geo, pos+1.05*n_geo)
 
-  east = pvs.Line(registrationName="E", Point1=pos, Point2=pos+e_geo)
-  east_tube = pvs.Tube(east, Radius=0.02)
+  east = pvs.Line(registrationName="  E_GEO Axis Line", Point1=pos, Point2=pos+e_geo)
+  east_tube = pvs.Tube(east, registrationName="  E_GEO Axis Tube", Radius=0.02)
   mvs.SetColor('green')
   pvs.Show(east_tube)
-  showlabel('E',pos+e_geo,pos+e_geo+0.05*n_geo)
+  showlabel('E', pos+e_geo, pos+e_geo+0.05*n_geo)
 
-  up = pvs.Line(registrationName="Z", Point1=pos, Point2=pos+z_geo)
-  up_tube = pvs.Tube(up, Radius=0.02)
+  up = pvs.Line(registrationName="  Z_GEO Axis Line", Point1=pos, Point2=pos+z_geo)
+  up_tube = pvs.Tube(up, registrationName="  Z_GEO Axis Tube", Radius=0.02)
   mvs.SetColor('red')
   pvs.Show(up_tube)
-  showlabel('Z',pos+z_geo,pos+z_geo+0.05*n_geo)
+  showlabel('Z', pos+z_geo, pos+z_geo+0.05*n_geo)
 
-  s = pvs.Sphere(Radius=0.05, Center=pos)
+  s = pvs.Sphere(registrationName=pos_label, Radius=0.05, Center=pos)
   pvs.Show(s)
 
 # Demo 1
-time = "2010-01-01T06:00:00"
-pos = (1, 0, 0)
+time = "2010-01-01T00:00:00"
+pos_GEO = (1, 0, 0)
 csys = "GEO"
-n_geo, e_geo, z_geo = nez(time, pos, csys)
+pos_GEO_label = f"$(r,\\lambda,\\phi)_{{GEO}}$={pos_GEO}"
+from hxform import hxform as hx
+pos = hx.transform(np.array(pos_GEO), time, 'GEO', csys, ctype_in="sph", ctype_out="car", lib='cxform')
+n_geo, e_geo, z_geo = nez(time, pos_GEO, csys)
 
-showaxis(pos, n_geo, e_geo, z_geo)
+showaxis(pos_GEO, n_geo, e_geo, z_geo, pos_label=pos_GEO_label)
 mvs.SetOrientationAxisLabel(Text="GEO")
 mvs.Earth(time=time, coord_sys=csys)
+mvs.SetTitle(f"{time} {pos_GEO_label}")
 mvs.LatLong(coord_sys=csys, coord_sys_view=csys)
 
 # Demo 2
 mvs.CreateViewAndLayout()
 
 csys = "GSM"
-time = "2009-01-01T03:00:00"
+time = "2010-01-01T12:00:00"
 #pos = (1., 18.907, 72.815) # Geographic r, lat, long of Colaba
 #pos = (1., 60., 0.)
-#pos = (1., 0, 0)
-#from hxform import hxform as hx
-#pos = hx.transform(np.array(pos), time, 'GEO', csys, ctype_in="sph", ctype_out="car", lib='cxform')
-pos = (1., 0, 0)
-mvs.Earth(time=time)
+pos_GEO = (1, 0, 0)
+mvs.SetTitle(f"{time} {pos_GEO_label}")
+
+from hxform import hxform as hx
+pos = hx.transform(np.array(pos_GEO), time, 'GEO', csys, ctype_in="sph", ctype_out="car", lib='cxform')
 n_geo, e_geo, z_geo = nez(time, pos, csys)
-showaxis(pos, n_geo, e_geo, z_geo)
+
+mvs.Earth(time=time)
+mvs.SetTitle(f"{time} {pos_GEO_label}")
+showaxis(pos, n_geo, e_geo, z_geo, pos_label=pos_GEO_label)
 mvs.SetOrientationAxisLabel(Text="GSM")
 mvs.LatLong(time=time, coord_sys="GEO", coord_sys_view=csys)
